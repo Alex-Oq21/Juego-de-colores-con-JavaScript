0b3d136367ed4b12f1c16249c694ffdfe26acb2e
Alexis <omaroquendo221@gmail.com>
2020-08-30 16:36:24 -0500

    Versión 2 de la actividad

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-var numeroCuadros = 6;
-var colores = ["rgb(255, 0, 0)", "rgb(255, 0, 255)", "rgb(255, 225, 0)", "rgb(255, 0, 255)", "rgb(0, 255, 255)", "rgb(0, 255, 0)"];
-var SeleccionColor;
-var cuadrados = document.querySelectorAll(".cuadrado");
-var botonReiniciar = document.querySelector("#reiniciar");
-var botonesModos = document.querySelector(".modo")
-
-systemMessageDisplay.innerHTML = SeleccionColor;
-for (i = 0; i < cuadrados.length; i++) {
-    cuadrados[i].style.backgorundColor = colores[i]
-}
-
-//Evtno click
-for (i = 0; i <= cuadrados.length; i++) {
-    cuadrados[i].addEventListener('click', function() {
-        alert('opción clikeada');
-    })
-}
+var numSquare = 6;
+var pickedColor;
+var squares = document.querySelectorAll(".square");
+var systemMessageDisplay = document.querySelector("#default-code");
+var userMessageDisplay = document.querySelector("#user-code");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+var header = document.querySelector("#header");
