fd269396e512b234206c9f5b1b177cd46c385c56
Alexis <omaroquendo221@gmail.com>
2020-08-30 16:55:09 -0500

    A la espera de agregar funciones.

--- app.js
+++ app.js
@@ -6,3 +6,29 @@ var userMessageDisplay = document.querySelector("#user-code");
 var resetButton = document.querySelector("#reset");
 var modeButtons = document.querySelectorAll(".mode");
 var header = document.querySelector("#header");
+var colors = generateRandomColors(6);
+pickedColor = pickColor();
+
+
+systemMessageDisplay.innerHTML = pickedColor;
+for (i = 0; i < squares.length; i++) {
+    //Aplica color a los cuadros
+    squares[i].style.backgroundColor = colors[i]
+        //Evento click en los cuadros
+    squares[i].addEventListener('click', function() {
+        //Mostrar valor actual
+        userMessageDisplay.innerHTML = this.style.backgroundColor;
+        //Selecciona el color correcto
+        var clickedColor = this.style.backgroundColor;
+        //Comprobar si el color seleccionado coincide
+        if (pickedColor === clickedColor) {
+            header.style.backgroundColor = pickedColor;
+            changeColors(pickedColor);
+        }
+        //Si el usuario selecciona el color incorrecto
+        else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.text = "Color incorrecto";
+        }
+    })
+};
